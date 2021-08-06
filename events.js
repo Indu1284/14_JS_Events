8502c570e2ed2fd40492b9e3b88b081da6fd870f
Indu <indurevathy@gmail.com>
2021-08-06 12:39:10 +0200

    button updated

--- events.js
+++ events.js
@@ -1,7 +1,39 @@
 
 /********  Variablen **********/
+//const btn = document.getElementById("trigBtn");
+const btn = document.body;
+let status = true;
+
+btn.addEventListener("click",actOnBtnEvent);
+
+function actOnBtnEvent() {
+    ausgabe("Hi");
+}
+
+/***** Business-Logic | Toggle ***** */
+function toggleStatus() {
+    status = !status;
+    ausgabe(status);
+}
+
+/*******  Änderung in View-Schicht *******/
+
+// Modul : Umschaltung Klassenamen | Test:
+switchClassName("night");
+//switchClassName("day");
+function switchClassName(modeStr) {
+    document.body.className = modeStr;
+    document.body.children[0].className = modeStr;
+    document.body.children[1].className = modeStr;
+}
 
 
+switchBtnTxt("night");
+switchBtnTxt("night");
+function switchBtnTxt(modeStr) {
+    const btn = document.getElementById("trigBtn");
+    btn.innerHTML = modeStr;
+    }
 /* Tools */
 function ausgabe(outputStr) {
     console.log(outputStr);
